1c76dbc0cd956288dea353cef625fc1e53c4e910
sirajshaikh-dev <ss1425306@gmail.com>
2025-07-11 22:35:16 +0530

    api response and error handling

--- Video_streaming_platform/src/index.js
+++ Video_streaming_platform/src/index.js
@@ -1,19 +1,17 @@
 import 'dotenv/config'
 import connectDB from './db/db.js';
-
+import express from "express"
+const app=express()
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log('MongoDB connection failed',error)
+})
 
 
 
---
+++ Video_streaming_platform/src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise
+        .resolve(requestHandler(req,res,next))
+        .catch((err)=>{next(err)})
+    }
+}
+
+
+// Method2: Using try catch
+//asyncHandler is higher order func: accept func as parameter and can return
+// const asyncHandler= (fn)=>{()=>{}}
+// const asyncHandler= (fn)=>{async()=>{}}
+/*
+const asyncHandler= (fn)=>async(err,req,res,next)=>{
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success:false,
+            message:err.message
+        })
+    }
+}
+*/
