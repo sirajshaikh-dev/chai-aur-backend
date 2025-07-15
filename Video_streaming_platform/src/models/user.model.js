3342cf5dac16fbe09694af47bdf3adad04b7fa5f
sirajshaikh-dev <ss1425306@gmail.com>
2025-07-15 22:11:13 +0530

    Complete: Implement user authentication with JWT and bcrypt; add pagination to video model

--- Video_streaming_platform/src/models/user.model.js
+++ Video_streaming_platform/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema, schema } from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 const userSchema = new schema(
   {
@@ -17,7 +19,7 @@ const userSchema = new schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -46,5 +48,42 @@ const userSchema = new schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre('save',function(){
+  if (!this.isModified('password')) return next()
+  this.password= bcrypt.hash(this.password,10)
+  next()  
+})
+
+userSchema.methods.isPasswordCorrect=async function (password) {
+  return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken= function(){ //can add async if takes time, usually it dont takes much time
+  return jwt.sign(
+    {
+      _id:this._id,
+      email:this.email,
+      username:this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken= function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 export const User = mongoose.model("User", userSchema);
